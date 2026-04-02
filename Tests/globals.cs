3d74bcf566b610f0bfef7714037fb8c237456564
Nicholas Petersen <copernicus365@gmail.com>
2026-04-02 18:08:41 -0400

    test proj proj updates

---
+++ Tests/globals.cs
@@ -0,0 +1,7 @@
+global using DotNetXtensions;
+global using DotNetXtensions.Html;
+global using Xunit;
+global using System.Text;
+
+global using static Xunit.Assert;
+global using static System.Console;
